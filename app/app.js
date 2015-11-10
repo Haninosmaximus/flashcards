57e5eedc119aefb8c95a8ff0f9ee376503966202
Jordan Rhea <rheajt@gmail.com>
2015-11-11 00:06:58 +0200

    createView reads in the csv file to json

--- app/app.js
+++ app/app.js
@@ -6,6 +6,10 @@ app.config(['$routeProvider', function($routeProvider) {
       templateUrl: 'app/components/index/indexView.html',
       controller: 'IndexCtrl'
     })
+    .when('/create', {
+      templateUrl: 'app/components/create/createView.html',
+      controller: 'CreateCtrl'
+    })
     .otherwise({
       redirectTo: '/'
     });
@@ -14,5 +18,50 @@ app.config(['$routeProvider', function($routeProvider) {
 app.controller('IndexCtrl', ['$scope', function($scope) {
 
   $scope.data = 'welcome';
+
+}]);
+
+app.controller('CreateCtrl', ['$scope', function($scope) {
+  $scope.filesChanged = function(elm) {
+    $scope.csvFile = elm.files[0];
+    $scope.$apply();
+  }
+  $scope.create = function() {
+    Papa.parse($scope.csvFile, {
+      header: true,
+      complete: function(result) {
+        console.log(typeof result.data);
+        console.log(result.data);
+
+        $scope.jsonResult = filterCSV(result.data);
+        $scope.$apply();
+      }
+    });
+
+    function filterCSV(data) {
+      var answerObj = data.shift();
+      var newArray = [];
+
+      for(var i = 0; i < data.length; i++) {
+
+        for(var key in data[i]) {
+          if(!data[i].hasOwnProperty(key)) {
+            continue;
+          }
+
+          if(data[i][key] == 1) {
+            delete data[i][key];
+          } else if(data[i][key] == 0) {
+            data[i][key] = answerObj[key];
+          }
+
+        }
+        newArray.push(data[i]);
+
+      }
+      return newArray;
+
+    }
+  }
 
 }])
